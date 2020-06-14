b7b0f6b0f44cd5ef4e0e5aaa03572a89d87c331c
Evgeny Panteleev <evg29111@gmail.com>
2020-06-14 16:30:09 +0500

    Конец работы над усложненным 4 уроком

--- script.js
+++ script.js
@@ -1,41 +1,15 @@
 "use strict";
-let lang = ['ru', 'en'],
-    rus = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб','вс'],
-    eng = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat','sun'],
-    mes =  confirm('Дни недели вывезди на русском (ок) или английском (отмена)?');
-console.log('--------Первое задание-------');
-if (mes === true) {
-    console.log('if: ', String(rus)); 
-} else {
-    console.log('if: ', String(eng)); 
+let arg = '    Программа для проверки длины строки на количество    ';
+function argument(data) {
+    if(typeof(data) !== 'string') {
+        alert('аргумент НЕ строка');
+        return 0;
+    }
+    console.log('Строка до преобразования:', data );
+    console.log('Длина строки до преобразования: ', data.length);
+    console.log('Строка после преобразования:', data.trim() );
+    console.log('Длина строки после преобразования: ', data.trim().length);
+    return data.trim().substring(0, 30);
 }
 
-switch (mes) {
-    case true: 
-        console.log('switch: ', String(rus)); 
-        break;
-    case false:
-        console.log('switch: ', String(eng)); 
-        break;
-    default: alert('Что-то пошло не так');
-}
-
-let matr = [
-    ['mon', 'tue', 'wed', 'thu', 'fri', 'sat','sun'],
-    ['пн', 'вт', 'ср', 'чт', 'пт', 'сб','вс']    
-];
-
-console.log('Многомерный массив: ', String(matr[(Number(mes))]));
-
-console.log('--------Второе задание-------');
-
-let namePerson = prompt('Введите имя: ');
-
-let message = (namePerson === 'Артем') ? 'директор' :
-                (namePerson === 'Максим') ? 'преподаватель':
-                'студент';
-let ending = (namePerson === 'Артем' || namePerson === 'Максим') ? 'а: ' : ':';
-
-let post = (ending === 'а: ') ? 'Должность ' : 'Должность';
-
-console.log(post + namePerson + ending, message);
+console.log('Результат выполнения задачи:', argument(arg) + '...');
