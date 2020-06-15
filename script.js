9b51627eba2c2ad7c4121ff090b4b12ad8441a66
Evgeny Panteleev <evg29111@gmail.com>
2020-06-16 00:17:13 +0500

    Конец 5 усложненного урока

--- script.js
+++ script.js
@@ -1,15 +1,51 @@
 "use strict";
-let arg = '    Программа для проверки длины строки на количество    ';
-function argument(data) {
-    if(typeof(data) !== 'string') {
-        alert('аргумент НЕ строка');
-        return 0;
+/* -----------1 задание ------------ */
+console.log('----- 1 задание ------');
+
+let num;
+let buf = [];
+let isNumber = function (n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+};
+
+let arr = ['25', '35', '45', '65', '261', '42', '66'];
+
+function record() {
+    for (let i = 0; i < 7; i++) {
+        num = prompt('Введите простое ' + (i + 1) + '-е' + ' многозначные число: ');
+        while (!isNumber(num)) {
+            num = prompt('Введите простое ' + (i + 1) + '-е' + ' многозначные число еще раз: ');
+        }
+        arr[i] = num;
     }
-    console.log('Строка до преобразования:', data );
-    console.log('Длина строки до преобразования: ', data.length);
-    console.log('Строка после преобразования:', data.trim() );
-    console.log('Длина строки после преобразования: ', data.trim().length);
-    return data.trim().substring(0, 30);
+    return arr;
 }
 
-console.log('Результат выполнения задачи:', argument(arg) + '...');
+num = record();
+
+console.log('Начальный массив: ', String(num));
+
+function check() {
+    for (let i = 0, j = 0; i < 7; i++) {
+        if (String(num[i]).startsWith('2') || String(num[i]).startsWith('4')) {
+            buf[j] = num[i];
+            j++;
+        }
+    }
+    return buf;
+}
+console.log('Числа начианются на 2 и 4: ', String(check()));
+
+/* -----------2 задание ------------ */
+console.log('----- 2 задание ------');
+let numb, n = 100;
+for (let i = 2; i < n; i++) {
+    for (let j = 2; j <= i; j++) {
+        if (i === j) {
+            console.log('Делитель числа ' + i + ': 1 и ' + j);
+        }
+        if (i % j === 0) {
+            break;
+        }
+    }
+}
