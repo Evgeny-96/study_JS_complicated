459cbf7e9a3d57c71edda1b03e84c2d87fc65b54
Evgeny Panteleev <evg29111@gmail.com>
2020-06-22 21:07:31 +0500

    Урок 7 усложненный

--- script.js
+++ script.js
@@ -1,57 +1,15 @@
 "use strict";
 
-let isNumber = function (n) {
-    return !isNaN(parseFloat(n)) && isFinite(n);
-};
-
-function question(num) {
-    let i = 10;
-
-    function question1() {
-        let validate;
-        let ques = prompt('Угадай число от 1 до 100');
-        if (Boolean(ques) === false && ques !== '') {
-            alert('Вы закончили игру!');
-            return 0;
-        } else if (!isNumber(ques)) {
-            alert('Введи число!');
-            question1();
-        } else if (num < ques) {
-            i--;
-            if (i === 0) {
-                validate = confirm('Попытки закончились, хотите сыграть еще?');
-                if (validate) {
-                    question(num);
-                } else if(!validate) {
-                    return 0;
-                }
-            }
-            alert('Загаданное число меньше, осталось попыток: ' + i);
-            question1();
-        } else if (num > ques) {
-            i--;
-            if (i === 0) {
-                validate = confirm('Попытки закончились, хотите сыграть еще?');
-                if (validate) {
-                    question(num);
-                } else if(!validate) {
-                    return 0;
-                }
-            }
-            alert('Загаданное число больше, осталось попыток: ' + i);
-            question1();
-        } else {
-            validate = confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?');
-            if (validate) {
-                question(num + 3);
-            } else {
-                return 0;
-            }
-
-        }
-
+let week = ['вс', 'пн', 'вт', 'ср', 'чт', 'пт', 'сб'];
+let date1 = new Date();
+for (let i = 0; i < week.length; i++) { 
+    if (i === 0 || i === 6) {
+        document.write('<i style = "color: red;">' + week[i] + '</i>' + '<br>');
+    } else if ( i === date1.getDay()) {
+        document.write('<strong style = " color: darkgreen;">' + week[i]  + '</strong>' + '<br>');
+    } else {
+        document.write(week[i] + '<br>');
     }
-    question1();
 }
 
-question(20);
+
