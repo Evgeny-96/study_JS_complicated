d5926a6cc33c5150427e3c8710d61b6c0b465f2e
Evgeny Panteleev <evg29111@gmail.com>
2020-07-05 12:10:53 +0500

    Урок 13 усложненный

---
+++ script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+let textColor3 = document.querySelector('.text-color-3'),
+    textButton = document.querySelector('.text-button'),
+    body = document.querySelector('body'),
+    rendColor, result;
+
+    function rend() {
+        rendColor = Math.random();
+        rendColor *= 100000000;
+        result = Math.floor(rendColor).toString(16);
+        textColor3.textContent = '#' + result;
+        body.style.backgroundColor = '#' + result;
+        console.log('body.style.backgroundColor: ', body.style.backgroundColor);
+    }
+    textButton.addEventListener('click', rend);
