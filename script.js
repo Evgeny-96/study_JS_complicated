c330ff4940c72da8d0dc76bc5b9ded0e1978f841
Evgeny Panteleev <evg29111@gmail.com>
2020-07-11 10:16:10 +0500

    Второе задание урока 15

---
+++ script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+class First {
+    hello() {
+        console.log('Привет я метод родителя!');
+    }
+}
+
+class Second extends First {
+    hello1() {
+        this.hello();
+        console.log('А я наследуемый метод!');
+    }
+
+}
+
+const first = new First();
+const second = new Second();
+second.hello1();
